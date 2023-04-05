9f1305a5bb4e7ed4da54e8783d3dd69b4f6d6326
AnthonyZhang2 <76865712+AnthonyZhang2@users.noreply.github.com>
2023-04-04 21:10:01 -0500

    Changed category and post relationship
    Assigment 7

--- mysite/blogging/admin.py
+++ mysite/blogging/admin.py
@@ -1,6 +1,31 @@
 from django.contrib import admin
 from blogging.models import Post, Category
 
+"""
+class AuthorAdmin(admin.ModelAdmin):
+    pass
 
 admin.site.register(Post)
 admin.site.register(Category)
+
+"""
+
+
+class InlineModelAdmin(admin.TabularInline):
+    model = Category.posts.through
+
+
+class CategoryModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    inlines = [InlineModelAdmin, ]
+    exclude = ('posts',)
+
+
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ('title', 'text', 'author', 'published_date')
+    inlines = [InlineModelAdmin, ]
+
+
+admin.site.register(Category, CategoryModelAdmin)
+admin.site.register(Post, PostModelAdmin)
+
